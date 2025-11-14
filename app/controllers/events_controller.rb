c237c95a03deec1c3d2e4f8808352c2832040ce9
Moaiz Abrar <moaiz-abrar@uiowa.edu>
2025-11-13 20:14:45 -0600

    Added add_event controller method, created view for showing the events page with a create new event button

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,8 +14,23 @@ class EventsController < ApplicationController
   end
 
   def add_event
+    title = params["title"]
+    event_date = params["event_date"]
+    location = params["location"]
+    budget = params["budget"]
+    theme = params["theme"]
 
+    if title.blank? || event_date.blank? || location.blank? || budget.blank? || theme.blank?
+      flash[:alert] = "Please fill out all the fields"
+      redirect_to new_event_path and return
+    end
+    @event = Event.new(title: title, location: location, budget: budget, theme: theme, user_id: session[:user_id])
+    if @event.save
+      flash[:notice] = "Event created successfully"
+      redirect_to events_path
+    else
+      flash[:alert] = "Failed to create event"
+      redirect_to new_event_path
+    end
   end
-
-
 end
