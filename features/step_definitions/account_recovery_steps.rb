f00cd92283403ee41d81ef4764e8bd2ea7b5abe3
kbogott <kbogott@uiowa.edu>
2025-11-20 15:52:09 -0600

    Refactor ; recovery fixed

--- features/step_definitions/account_recovery_steps.rb
+++ features/step_definitions/account_recovery_steps.rb
@@ -6,14 +6,14 @@ When("I enter {string} into the identifier box") do |identifier|
 end
 
 When("I press the Send Recovery Instructions button") do
-  click_button "SEND RECOVERY INSTRUCTIONS"
+  click_button "Send Recovery Instructions"
 end
 
 # Then
 Then("I should stay on the Account Recovery page") do
-  expect(page).to have_current_path(forgot_path)
+  expect(page).to have_current_path(recovery_path)
 end
 
 Then("I should be redirected to the login page") do
-  expect(page).to have_current_path(login_path)
+  expect(page).to have_current_path(logi_path)
 end
