aaa892c705cfcac12e20425a2debc8fa17fe6ed2
kbogott <kbogott@uiowa.edu>
2025-11-29 15:43:01 -0600

    Initial Creation / Setup for resetting pass through email link. Created step definitions and user story. Need to implement Mailtrap email system and correctly tie each reset token within the route.

--- app/controllers/recovery_controller.rb
+++ app/controllers/recovery_controller.rb
@@ -29,30 +29,30 @@ class RecoveryController < ApplicationController
     # Do NOT reveal whether account exists always give message to not show what emails are associated with accounts (security)
      flash[:notice] = "If this account exists, recovery instructions have been sent."
      redirect_to login_path
-    end
+  end
 
   # -------
   # Handle setting a new password
   # -------
   def edit
-    @token = params[:token] # Grab token
-    @user = User.find_by(reset_token: @token) # Find user
+    @token = params[:token] # Grab token & Find user
+    @user = User.find_by(reset_token: @token)
 
     # IF NO USER FOUND || TOKEN INVALID || EXPIRED
-    unless @user
+    if @user.nil? || token_expired?(@user)
       flash[:alert] = "Invalid or expired recovery link."
-    redirect_to recovery_path
+      return redirect_to recovery_path
     end
     #If the token is valid -->  app/views/recovery/edit.html.erb
   end
 
   def update
-    token = params[:token]    # Read in token sent from form
-    @user = User.find_by(reset_token: token) # Find User
+    #  Find User & Read in token sent from form
+    token = params[:token]
+    @user = User.find_by(reset_token: token)
 
-    # no matching user
-    unless
-      @user
+    # Invalid or Expired Token
+    if @user.nil? || token_expired?(@user)
       flash[:alert] = "Invalid or expired recovery link."
       return redirect_to recovery_path
     end
@@ -61,16 +61,21 @@ class RecoveryController < ApplicationController
     new_password = params[:password]
 
     # Check Password Validity
-    if new_password.blank? || new_password.size < 8
+    if new_password.blank? || new_password.size < 6
       flash[:alert] = "Invalid Password"
       return redirect_to recovery_reset_path(token: token) # Keep the user on the reset page --> save the token in the URL
     end
 
-    # Update Users password and clear reset token so it cant be used more than once
+
+    # SUCCESS:  Update Users password and clear reset token so it cant be used more than once
     @user.update(password: new_password, reset_token: nil)
-
     flash[:notice] = "Password Successfully Updated! Please log in."
-
     redirect_to login_path
   end
+
+  # Helper Method for determining Validity of Reset Token (15 Min until Invalid)
+  private
+  def token_expired?(user)
+    user.reset_sent_at.nil?  ||  user.reset_sent_at < 15.minutes.ago
+    end
 end
