7779db8e7a845ef7e58372fa2ca5dc32fb0ca5ac
kbogott <kbogott@uiowa.edu>
2025-11-28 23:24:09 -0600

    Mailer SKeleton Code: TODO

--- app/mailers/recovery_mailer.rb
+++ app/mailers/recovery_mailer.rb
@@ -3,16 +3,17 @@ class RecoveryMailer < ApplicationMailer
   default from: "group7@giftwise.com"  # Can be changed
 
 
-  def reset_email(user)
-    @user = user
-    @token = @user.reset_token
+  # TODO FIX
+  # def reset_email(user)
+  #  @user = user
+  #  @token = @user.reset_token
 
     # http://localhost:3000/recovery/reset?token=XYZ
-    @reset_url = recovery_reset_url(token: @token)
+  #    @reset_url = recovery_reset_url(token: @token)
 
-    mail(
-      to: @user.email,
-      subject: "Your GiftWise Password Reset Instructions"
-    )
+  # mail(
+  #   to: @user.email,
+  #    subject: "Your GiftWise Password Reset Instructions"
+  #  )
+  #end
   end
-end
