405f7d997b757507acee136f788e0fd2ea77d885
Katherine Wright <katherine.wright@uiowa.edu>
2025-12-01 13:14:58 -0600

    WIP: user profile changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -85,7 +85,7 @@ class UsersController < ApplicationController
 
   # PATCH/PUT /users/1 or /users/1.json
   def update
-    add    if @user.update(user_params)
+    if @user.update(user_params)
       redirect_to @user, notice: "Account updated successfully!"
     else
       flash.now[:alert] = "Account not updated!"
