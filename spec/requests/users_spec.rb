51b7f5acad5cb3ae2357e177ca99163cda031d11
Katherine Wright <kiwrght@uiowa.edu>
2025-12-03 22:21:01 -0600

    Confirmed the Delete user specs passed and the Delete User Button does work

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,28 +1,31 @@
 require "rails_helper"
 
-RSpec.describe "Users Controller", type: :controller do
-  let!(:user) {User.create!(username: "testuser", email: "test@example.com", password: "password")}
-
+RSpec.describe "Users", type: :request do
+  let!(:user) { User.create!(username: "testuser", email: "test@example.com", password: "password") }
+  before do
+    post login_path, params: { username: user.username, password: "password" }
+  end
   describe "DELETE /users/:id" do
     context "with correct username confirmation" do
-      it "deletes the use rand redirects to the root" do
-        expect{
-          delete user_path(user), params: {id: user.id, confirmation_username: "testuser"}
+      it "deletes the user and redirects to login" do
+        expect {
+          delete user_path(user), params: { confirm_username: "testuser" }
         }.to change(User, :count).by(-1)
 
-        expect (response).to redirect_to(root_path)
-        expect(flash[:notice]).to eq( "User deleted successfully.")
+        expect(User.find_by(id: user.id)).to be_nil
+        expect(response).to redirect_to(login_path)
+        expect(flash[:notice]).to eq("User deleted successfully.")
       end
     end
 
     context "with incorrect username confirmation" do
       it "does not delete the user and redirects back" do
-        expect{
-          delete user_path(user) , params: {id: user.id, confirmation_username: "wrongname"}
+        expect {
+          delete user_path(user), params: { confirm_username: "wrongname" }
         }.not_to change(User, :count)
 
-        expect (response).to redirect_to(users_path(user))
-        expect(flash[:alert]).to eq( "User confirmation failed. Account not deleted." )
+        expect(response).to redirect_to(user_path(user))
+        expect(flash[:alert]).to eq("Username confirmation failed. Account not deleted.")
       end
     end
   end
